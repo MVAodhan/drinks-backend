5f8e24a16fbc7f8aa75b4eedbd2c21b4e54001c0
MVAodhan <hamilton.aodhan@gmail.com>
2021-02-20 21:22:21 +1300

    drinks-backend first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b81f92416e625ba27deb4cf476bb1a92'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2fe4f09f-892c-4fc1-9365-9416b86d9bb0'
+};
